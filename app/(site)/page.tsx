6e510fe0642fa1eb67f9611f06521e1564ee7cb0
Thinh La <thinhlq@smartosc.com>
2024-02-28 23:56:11 +0700

    Fix deploy

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,19 +1,14 @@
 import React from "react";
 import Banner from "@/app/components/Homepage/Banner";
-import { createReader } from "@keystatic/core/reader";
-import keystaticConfig from "@/keystatic.config";
-import PostGrid from "../components/Post/PostGrid";
-
-const reader = createReader(process.cwd(), keystaticConfig);
+import PostGrid from "@/app/components/Post/PostGrid";
+import { Reader } from "@/app/keystatic/utils";
 
 export default async function HomePage() {
-	const homePage = await reader.singletons.homepage.read();
-
-	//console.log(homePage);
+	const homePage = await Reader.singletons.homepage.read();
 
 	return (
 		<div className="homepage">
-			{/* {homePage?.banner && homePage.banner.length > 0 && <Banner props={homePage.banner} />} */}
+			{homePage?.banner && homePage.banner.length > 0 && <Banner props={homePage.banner} />}
 			<div className="container py-10">
 				<PostGrid />
 			</div>
