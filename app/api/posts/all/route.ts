db0525f48c9bc68da57ab28aa05e1840e8a291f6
Thinh La <thinhlq@smartosc.com>
2024-03-07 13:08:48 +0700

    Update API Category

--- app/api/posts/all/route.ts
+++ app/api/posts/all/route.ts
@@ -1,26 +1,10 @@
 import { Reader, getCategoryBySlug } from "@/app/keystatic/utils";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function OPTIONS(request: Request) {
-	const allowedOrigin = request.headers.get("origin");
-	const response = new NextResponse(null, {
-		status: 200,
-		headers: {
-			"Access-Control-Allow-Origin": allowedOrigin || "*",
-			"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-			"Access-Control-Allow-Headers":
-				"Content-Type, Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version",
-			"Access-Control-Max-Age": "86400",
-		},
-	});
-
-	return response;
-}
-
 export async function GET(request: NextRequest) {
 	try {
 		const allPosts = await Reader.collections.posts.all();
-		const categories = await Reader.collections.categories.all();
+		//const categories = await Reader.collections.categories.all();
 
 		// const getCategory = (cate: readonly string[]) => {
 		// 	const category = cate.map(async (item: string) => {
