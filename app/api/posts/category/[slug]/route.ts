f2a5c3590f3cd625dc4e9df75f72f911b40f59f2
Thinh La <thinhlq@smartosc.com>
2024-03-07 13:48:59 +0700

    Update API Category

--- app/api/posts/category/[slug]/route.ts
+++ app/api/posts/category/[slug]/route.ts
@@ -4,27 +4,27 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(request: NextRequest) {
 	try {
 		const posts = await Reader.collections.posts.all();
+
 		const pathName = request.nextUrl.pathname;
 		const slug = pathName.substring(pathName.lastIndexOf("/") + 1) || "slug";
-
 		console.log(pathName);
 		console.log("Get API Posts Category slug = " + slug);
 
 		const postCategory = posts.filter((post) => post.entry.categories.includes(slug));
-
 		console.log(postCategory);
+
 		// if (!postCategory || postCategory.length === 0) {
 		// 	return NextResponse.json({ error: `Not found any Post with Category Slug: ${slug}` }, { status: 200 });
 		// }
 
-		const dataPosts = postCategory.map((post) => {
+		const dataPosts = posts.map((post) => {
 			return {
 				...post,
 				categorySlug: slug,
 			};
 		});
 
-		return NextResponse.json([dataPosts, posts]);
+		return NextResponse.json(dataPosts);
 	} catch (error: any) {
 		return NextResponse.json({ error: `An error occurred: ${error.message}` }, { status: 200 });
 	}
